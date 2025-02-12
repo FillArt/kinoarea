e6f00dd948b3fcd5ef4d7c4d568e1c42a0b67347
FillArt <artiom.filipopovschii@gmail.com>
2025-02-12 15:51:28 -0400

    feat(button): added icon button

--- src/widgets/Header/ui/Buttons/Buttons.tsx
+++ src/widgets/Header/ui/Buttons/Buttons.tsx
@@ -1,8 +1,14 @@
 import {ButtonBase} from "@/common/components/buttons/ButtonBase.tsx";
+import {ButtonIcon} from "@/common/components/buttons/ButtonIcon.tsx";
+
+import SearchIcon from "@/common/components/buttons/assets/search.svg"
 
 export const Buttons = () => {
     return (
-        <div className="text-smallFontSize">
+        <div className="flex text-smallFontSize gap-[12px]">
+            <ButtonIcon onClick={() => alert('Открыл поиск...')}>
+                <img src={SearchIcon}  alt=""/>
+            </ButtonIcon>
             <ButtonBase title='Войти' onClick={() => alert('Вошёл')} />
         </div>
     );
