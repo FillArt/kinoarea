a76f808891bc2b3e26477b6c9f26616d07796e7f
FillArt <artiom.filipopovschii@gmail.com>
2025-02-12 11:45:44 -0400

    style: basic tailwind.config.js

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,8 @@ function App() {
 
     return (
         <>
-            <h1 className="text-3xl font-bold text-red-500">Hello World!</h1>
+            <h1 className="text-3xl font-bold text-red-500 font-main ">Hello World!</h1>
+
         </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./index.html",
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                main: ['Qanelas', 'sans-serif']
+            },
+            colors: {
+                baseTextColor: "#FFFFFF",
+                decorTextColor: "#F2F60F",
+                subTextColor: "#3B486B",
+            },
+            backgroundColor: {
+                backgroundColor: "#1E2538",
 
+                accentButtonColor: "#F2F60F",
+                formElementColor: "#3657CB",
+                socialButtonColor: "#3C4767",
+
+                positiveReviewColor: "#57D043",
+                neutralReviewColor: "#FFF065",
+                negativeReview: "#E04141"
+            },
+
+            fontSize: {
+                defaultFontSize: '20px',
+                smallFontSize: '18px',
+                titleFontSize: '65px',
+                subtitleFontSize: '45px',
+                descriptionFontSize: '25px',
+            }
+        },
+        plugins: [],
+    }
+
+}
