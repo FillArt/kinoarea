3896f375c4e6dd50c8380e70c6ae363f717c2a40
FillArt <artiom.filipopovschii@gmail.com>
2025-02-17 22:29:50 -0400

    refactor(homePage): moved the logic to HomePage temporarily

--- src/pages/HomePage/sections/NowInCinema/ui/NowInCinema.tsx
+++ src/pages/HomePage/sections/NowInCinema/ui/NowInCinema.tsx
@@ -1,24 +1,25 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {SectionTitle} from "@/common/components/sections/SectionTitle.tsx";
 import {categoriesList} from "@/pages/HomePage/sections/NowInCinema/model/categoriesList.ts";
 import {CinemaList} from "@/pages/HomePage/sections/NowInCinema/ui/CinemaList";
-import {GenreAPI, NowInCinemaType} from "@/pages/HomePage/sections/NowInCinema/api/NowInCinemaAPI.types.ts";
 import {ButtonBase} from "@/common/components/buttons/ButtonBase.tsx";
-import {NowInCinemaAPI} from "@/pages/HomePage/sections/NowInCinema/api/NowInCinemaAPI.ts";
 import {CardMovieSkeleton} from "@/common/components/cards";
 import Icon from "@/common/components/buttons/assets/burgerWhite.svg";
 import {ButtonIcon} from "@/common/components/buttons/ButtonIcon.tsx";
+import {NowInCinemaType} from "@/pages/HomePage/sections/NowInCinema/api/NowInCinemaAPI.types.ts";
 
 export type Filter = "all" | "action" | "adventures" | "comedy" | "fantasy" | "thrillers" | "drama";
 
-export const NowInCinema = () => {
-    const [filter, setFilter] = useState<Filter>("all");
-    const [movies, setMovies] = useState<NowInCinemaType[]>([]);
-    const [allMovies, setAllMovies] = useState<NowInCinemaType[]>([]);
-    const [genres, setGenres] = useState<GenreAPI[]>([]);
+type NowInCinemaProps = {
+    movies: NowInCinemaType[],
+    loading: boolean,
+    allMoviesHandler: () => void,
 
+}
+
+export const NowInCinema = ({movies, loading, allMoviesHandler}: NowInCinemaProps) => {
+    const [filter, setFilter] = useState<Filter>("all");
     const [isShowButton, setIsShowButton] = useState<boolean>(true);
-    const [loading, setLoading] = useState<boolean>(true);
 
     const onClickHandler = (filter: Filter) => {
         setFilter(filter);
@@ -26,36 +27,9 @@ export const NowInCinema = () => {
     }
 
     const showMoreMovies = () => {
-        setMovies(allMovies)
+        allMoviesHandler()
         setIsShowButton(false)
     };
-
-    const genreMap: Record<number, string> = genres.reduce((acc, genre) => {
-        acc[genre.id] = genre.name;
-        return acc;
-    }, {} as Record<number, string>);
-
-
-    useEffect(() => {
-        NowInCinemaAPI.getGenres().then((r) => setGenres(r.data.genres));
-    }, []);
-
-    useEffect(() => {
-        if (genres.length === 0) return;
-
-        NowInCinemaAPI.getNowPlaying()
-            .then((r) => {
-                const formatMovies = r.data.results.map((movie) => ({
-                    ...movie,
-                    genres: movie.genre_ids?.map((id: number) => genreMap[id]),
-                }));
-
-                setAllMovies(formatMovies);
-                setMovies(formatMovies.slice(0, 8));
-                setLoading(false);
-            });
-
-    }, [genres]);
 
 
     return (
--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,11 +1,51 @@
 import {HomePageLayout} from "@/common/components/layouts";
 import {NowInCinema} from "@/pages/HomePage/sections/NowInCinema";
 import {NewTrailers} from "@/pages/HomePage/sections/NewTrailers";
+import {useCallback, useEffect, useState} from "react";
+import {GenreAPI, NowInCinemaType} from "@/pages/HomePage/sections/NowInCinema/api/NowInCinemaAPI.types.ts";
+import {NowInCinemaAPI} from "@/pages/HomePage/sections/NowInCinema/api/NowInCinemaAPI.ts";
 
 export const HomePage = () => {
+
+    const [movies, setMovies] = useState<NowInCinemaType[]>([]);
+    const [allMovies, setAllMovies] = useState<NowInCinemaType[]>([]);
+    const [genres, setGenres] = useState<GenreAPI[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    const genreMap: Record<number, string> = genres.reduce((acc, genre) => {
+        acc[genre.id] = genre.name;
+        return acc;
+    }, {} as Record<number, string>);
+
+    const allMoviesHandler = useCallback(() => {
+        setMovies(allMovies)
+    },[allMovies])
+
+    useEffect(() => {
+        NowInCinemaAPI.getGenres().then((r) => setGenres(r.data.genres));
+    }, []);
+
+    useEffect(() => {
+        if (genres.length === 0) return;
+
+        NowInCinemaAPI.getNowPlaying()
+            .then((r) => {
+                const formatMovies = r.data.results.map((movie) => ({
+                    ...movie,
+                    genres: movie.genre_ids?.map((id: number) => genreMap[id]),
+                }));
+
+                setAllMovies(formatMovies);
+                setMovies(formatMovies.slice(0, 8));
+                setLoading(false);
+            });
+
+    }, [genres]);
+
+
     return (
         <HomePageLayout>
-            <NowInCinema/>
+            <NowInCinema movies={movies} loading={loading} allMoviesHandler={allMoviesHandler} />
             <NewTrailers/>
         </HomePageLayout>
     );
