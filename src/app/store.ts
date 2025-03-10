664b9f75d87550a0c24b3c481f4b6213e5916cb0
FillArt <artiom.filipopovschii@gmail.com>
2025-03-10 12:21:09 -0300

    feat: added AppDispatch

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,20 +1,20 @@
 
-// import {configureStore} from "@reduxjs/toolkit";
-//
-// // export const store = configureStore({})
+import {configureStore} from "@reduxjs/toolkit";
+
 // export const store = configureStore({})
-//
-// export type RootState = ReturnType<typeof store.getState>
-//
-// export type AppDispatch = typeof store.dispatch
+export const store = configureStore({})
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
 
 // temporarily!!!!
-export const store = {
-    getState() {
-        return []
-    },
-    subscribe() {}
-}
+// export const store = {
+//     getState() {
+//         return []
+//     },
+//     subscribe() {}
+// }
 
 // @ts-expect-error: output for debugging the store
 window.store = store
