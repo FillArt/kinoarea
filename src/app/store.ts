90336d0f769f808255df91ee7546d8cc3d70781f
FillArt <artiom.filipopovschii@gmail.com>
2025-03-10 12:24:33 -0300

    feat: added AppSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,19 +2,19 @@
 import {configureStore} from "@reduxjs/toolkit";
 
 // export const store = configureStore({})
-export const store = configureStore({})
-
-export type RootState = ReturnType<typeof store.getState>
-
-export type AppDispatch = typeof store.dispatch
+// export const store = configureStore({})
+//
+// export type RootState = ReturnType<typeof store.getState>
+//
+// export type AppDispatch = typeof store.dispatch
 
 // temporarily!!!!
-// export const store = {
-//     getState() {
-//         return []
-//     },
-//     subscribe() {}
-// }
+export const store = {
+    getState() {
+        return []
+    },
+    subscribe() {}
+}
 
 // @ts-expect-error: output for debugging the store
 window.store = store
