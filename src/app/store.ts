612730723ec43c09a81a10f1c9abfb3511b89bab
FillArt <artiom.filipopovschii@gmail.com>
2025-03-12 15:15:43 -0300

    feat(PopularPeople): rewrote PopularPeople logic into slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,12 +3,14 @@ import {configureStore} from "@reduxjs/toolkit";
 import {NowInCinemaSlice} from "@/pages/home/sections/NowInCinema/model/NowInCinemaSlice.ts";
 import {NewTrailersSlice} from "@/pages/home/sections/NewTrailers/model/NewTrailersSlice.ts";
 import {PopularFilmsSlice} from "@/pages/home/sections/PopularFilms/model/PopularFilmsSlice.ts";
+import {PopularPeopleSlice} from "@/pages/home/sections/PopularPeople/model/PopularPeopleSlice.ts";
 
 export const store = configureStore({
    reducer: {
        [NowInCinemaSlice.name]: NowInCinemaSlice.reducer,
        [NewTrailersSlice.name]: NewTrailersSlice.reducer,
        [PopularFilmsSlice.name]: PopularFilmsSlice.reducer,
+       [PopularPeopleSlice.name]: PopularPeopleSlice.reducer,
    }
 })
 
--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,34 +1,16 @@
-import {useEffect, useState} from "react";
 import {HomePageLayout} from "@/shared/layouts";
 import {NowInCinema} from "@/pages/home/sections/NowInCinema/ui/NowInCinema.tsx";
 import {NewTrailers} from "@/pages/home/sections/NewTrailers/ui/NewTrailers.tsx";
 import {PopularFilms} from "@/pages/home/sections/PopularFilms/ui/PopularFilms.tsx";
 import {PopularPeople} from "@/pages/home/sections/PopularPeople/ui/PopularPeople.tsx";
-import {PeopleType} from "@/shared/types/PepoleType.ts";
-import {peopleAPI} from "@/shared/api/PeopleAPI.ts";
 
 export const HomePage = () => {
-    const [popularPersonDay, setPopularPersonDay] = useState<PeopleType[]>([]);
-    const [popularPersonWeek, setPopularPersonWeek] = useState<PeopleType[]>([]);
-
-    useEffect(() => {
-
-        const fetchPopularPerson = async (time: 'day' | 'week') => {
-            const res = await peopleAPI.getPopularPeople(time)
-            return res.data.results;
-
-        }
-
-        fetchPopularPerson('day').then((res) => setPopularPersonDay(res.slice(0,6)))
-        fetchPopularPerson('week').then((res) => setPopularPersonWeek(res.slice(0,6)))
-    }, []);
-
     return (
         <HomePageLayout>
             <NowInCinema />
             <NewTrailers />
             <PopularFilms />
-            <PopularPeople popularDay={popularPersonDay} popularWeek={popularPersonWeek} />
+            <PopularPeople />
         </HomePageLayout>
     );
 };
