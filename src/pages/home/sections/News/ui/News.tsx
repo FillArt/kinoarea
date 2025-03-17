0920921d75242993697e58a91064957a03ccb14b
FillArt <artiom.filipopovschii@gmail.com>
2025-03-17 14:04:05 -0300

    feat(News): created a fake api for news. I couldn't find anything free :(

--- src/pages/home/sections/News/ui/News.tsx
+++ src/pages/home/sections/News/ui/News.tsx
@@ -1,6 +1,12 @@
+import {useAppSelector} from "@/shared/hooks/useAppSelector.ts";
+import {NewsSelector} from "@/pages/home/sections/News/model/NewsSlice.ts";
 
 
 export const News = () => {
+
+    const news = useAppSelector(NewsSelector)
+    console.log(news)
+
     return (
         <section className="font-main bg-backgroundColor pt-6 mb-10 text-white">
             <div className="container max-w-container mx-auto">
