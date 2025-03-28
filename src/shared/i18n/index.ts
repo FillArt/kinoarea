7a97fb0e156d53671afc61d383a8251218f035c1
FillArt <artiom.filipopovschii@gmail.com>
2025-03-28 09:55:05 -0300

    refactor: moving local files

--- src/shared/i18n/index.ts
+++ src/shared/i18n/index.ts
@@ -3,20 +3,20 @@ import {initReactI18next} from "react-i18next";
 // import LanguageDetector from "i18next-browser-languagedetector"; // Автоопределение языка браузера
 
 // Now in Cinema
-import nowInCinemaEn from "./locales/en/nowInCinema.json"
-import nowInCinemaRu from "./locales/ru/nowInCinema.json";
+import nowInCinemaEn from "./locales/en/home/nowInCinema.json"
+import nowInCinemaRu from "./locales/ru/home/nowInCinema.json";
 
 // New Trailers
-import newTrailersEn from "./locales/en/newTrailers.json"
-import newTrailersRu from "./locales/ru/newTrailers.json";
+import newTrailersEn from "./locales/en/home/newTrailers.json"
+import newTrailersRu from "./locales/ru/home/newTrailers.json";
 
 // Popular Films
-import popularFilmsEn from "./locales/en/popularFilms.json"
-import popularFilmsRu from "./locales/ru/popularFilms.json";
+import popularFilmsEn from "./locales/en/home/popularFilms.json"
+import popularFilmsRu from "./locales/ru/home/popularFilms.json";
 
 // Popular People
-import popularPeopleEn from "./locales/en/popularPeople.json"
-import popularPeopleRu from "./locales/ru/popularPeople.json";
+import popularPeopleEn from "./locales/en/home/popularPeople.json"
+import popularPeopleRu from "./locales/ru/home/popularPeople.json";
 
 
 const defaultLanguage = import.meta.env.VITE_APP_LANGUAGE || "en"; // Язык по умолчанию
