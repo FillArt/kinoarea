3b13cbeb5ab8722ad32ea5f567f8245cdda65d45
FillArt <artiom.filipopovschii@gmail.com>
2025-03-31 14:26:43 -0300

    test: Added tests for PopularFilmsSlice

---
+++ src/pages/home/sections/PopularFilms/model/PopularFilmsSlice.test.ts
@@ -0,0 +1,54 @@
+import {beforeEach, describe, expect, it, vi} from "vitest";
+import {fetchPopularMoviesTC, PopularFilmsReducer} from "./PopularFilmsSlice";
+import {MovieType} from "@/shared/types/MovieType";
+
+// Мокаем API
+vi.mock("@/shared/api/MovieAPI", () => ({
+    movieAPI: {
+        getPopular100: vi.fn(),
+    },
+}));
+
+describe("PopularFilmsSlice", () => {
+    let initialState: MovieType[];
+
+    beforeEach(() => {
+        initialState = [];
+    });
+
+    // Тест на начальное состояние
+    it("should return the initial state", () => {
+        // @ts-expect-error - for test with undefined type action
+        expect(PopularFilmsReducer(undefined, { type: undefined })).toEqual(initialState);
+    });
+
+    it("should update state with sorted unique movies when fetchPopularMoviesTC.fulfilled", () => {
+        const mockMovies = [
+            { id: 1, title: "Movie 1", vote_average: 8.5 },
+            { id: 2, title: "Movie 2", vote_average: 7.2 },
+            { id: 3, title: "Movie 3", vote_average: 9.0 },
+            { id: 1, title: "Movie 1", vote_average: 8.5 }, // Дубликат
+        ] as MovieType[];
+
+        const newState = PopularFilmsReducer(initialState, {
+            type: fetchPopularMoviesTC.fulfilled.type,
+            payload: [{ results: mockMovies }],
+        });
+
+        expect(newState).toEqual([
+            { id: 3, title: "Movie 3", vote_average: 9.0 },
+            { id: 1, title: "Movie 1", vote_average: 8.5 },
+            { id: 2, title: "Movie 2", vote_average: 7.2 },
+        ]);
+    });
+
+    // Тест на отклоненный запрос
+    it("should return initial state when fetchPopularMoviesTC is rejected", () => {
+        const newState = PopularFilmsReducer(initialState, {
+            type: fetchPopularMoviesTC.rejected.type,
+            error: "Network error",
+        });
+
+        expect(newState).toEqual([]);
+    });
+});
