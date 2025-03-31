d8e7311073f810ad9229de269ce3b93aebef3fcb
FillArt <artiom.filipopovschii@gmail.com>
2025-03-30 21:17:43 -0300

    test: configured vitest to work

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from "node:path";
+import {configDefaults} from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    exclude: [...configDefaults.exclude, "node_modules"],
+  },
   base: '/kinoarea/',
   resolve: {
     alias: {
