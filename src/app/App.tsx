3cd39c41f173e7520388682c1af280b27df3ada4
FillArt <artiom.filipopovschii@gmail.com>
2025-04-09 16:39:11 -0300

    temp: added page zoom out for ease of development

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,11 @@
 import './App.css'
 import {HomePage} from "@/pages/home";
+import {useEffect} from "react";
 
 export const App = () => {
+    useEffect(() => {
+        document.body.style.zoom = "0.8"; // 80% zoom
+    }, []);
 
     return (
         <>
