2dd3b2604bc7f76821a9263b24c06cc10df245d2
Artiom Filipopovschii <artiom.filipopovschii@gmail.com>
2025-04-26 16:46:20 -0300

    style: minor fix padding

--- src/widgets/Subscription/ui/Subscription.tsx
+++ src/widgets/Subscription/ui/Subscription.tsx
@@ -13,7 +13,7 @@ export const Subscription = () => {
                 className="container max-w-container mx-auto">
 
                 <div
-                    className="tabletLg:pt-[75px] phone:pt-[51px] pt-[26px] tabletLg:pb-[105px] phone:pb-[50px] pb-[25px] rounded-[10px]"
+                    className="tabletLg:pt-[75px] phone:pt-[51px] p-[26px] tabletLg:pb-[105px] phone:pb-[50px] pb-[25px] rounded-[10px]"
                     style={{backgroundImage: `linear-gradient(rgba(29, 58, 160, 0.9), rgba(29, 58, 160, 0.9)), url(${BgImage})`}}>
                     <div className=" w-full flex items-center flex-col mx-auto text-white">
                         <SubscriptionTitle/>
