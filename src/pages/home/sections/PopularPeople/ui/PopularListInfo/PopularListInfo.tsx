6b00468ed6298bc0022fe2202e2880f11c0de4ae
Artiom Filipopovschii <artiom.filipopovschii@gmail.com>
2025-04-26 11:31:51 -0300

    style(PopularPeople): added hover effects for elements of this section

--- src/pages/home/sections/PopularPeople/ui/PopularListInfo/PopularListInfo.tsx
+++ src/pages/home/sections/PopularPeople/ui/PopularListInfo/PopularListInfo.tsx
@@ -21,7 +21,7 @@ export const PopularListInfo = ({data}: PopularListInfoProps) => {
                 <div key={person.id}
                      className={`flex tabletLg:pb-[15px] pb-[10px] tabletLg:pt-[15px] pt-[10px] justify-between items-center ${index === data.length - 1 ? '' : 'border-b-2 border-[#1E2538]'}`}> {/* добавлен key для оптимизации React */}
                     <div className="">
-                        <h3 className="tabletLg:text-[20px] text-[15px] font-bold">{person.name}</h3>
+                        <h3 className="tabletLg:text-[20px] text-[15px] font-bold hover:text-[#3657CB] cursor-pointer">{person.name}</h3>
                         <h4 className="tabletLg:text-[15px] text-[11px] opacity-50 text-[#3B486B]">{person.original_name}</h4>
                         <span className="text-decorTextColor tabletLg:text-[14px] text-[11px]">87 лет</span>
                     </div>
--- src/pages/home/sections/PopularPeople/ui/PopularPhotoInfo/PopularPhotoInfo.tsx
+++ src/pages/home/sections/PopularPeople/ui/PopularPhotoInfo/PopularPhotoInfo.tsx
@@ -1,4 +1,5 @@
 import {PeopleType} from "@/shared/types/PepoleType.ts";
+import {ButtonBase} from "@/shared/ui/buttons/ButtonBase.tsx";
 
 type PopularPhotoInfoProps = {
     data: PeopleType | null
@@ -18,13 +19,26 @@ export const PopularPhotoInfo = ({data, place}: PopularPhotoInfoProps) => {
         : {};
 
     return (
-        <div style={imgStyle} className="tablet:h-full phone:h-[321px] h-[179px] tabletLg:py-[12px] py-[7px] tabletLg:px-[16px] px-[10px] flex flex-col justify-between rounded-[10px]">
-            <span className="tabletLg:text-[15px] text-[12px] text-decorTextColor">{place}</span>
-            <div className="tabletLg:mx-[20px] phone:mx-[15px] tabletLg:mb-[25px] mb-[12px] leading-tight">
-                <h3 className="tabletLg:text-[27px] text-[20px] font-bold">{data?.name}</h3>
-                <h4 className="tabletLg:text-[20px] text-[15px] opacity-50">{data?.original_name}</h4>
-                <span className="text-decorTextColor tabletLg:text-[15px] text-[12px]">57 лет</span>
+        <div className="group relative tablet:h-full phone:h-[321px] h-[179px] cursor-pointer">
+            <div style={imgStyle} className="tablet:h-full phone:h-[321px] h-[179px] tabletLg:py-[12px] py-[7px] tabletLg:px-[16px] px-[10px] flex flex-col justify-between rounded-[10px]">
+                <span className="tabletLg:text-[15px] text-[12px] text-decorTextColor">{place}</span>
+                <div className="tabletLg:mx-[20px] phone:mx-[15px] tabletLg:mb-[25px] mb-[12px] leading-tight">
+                    <h3 className="tabletLg:text-[27px] text-[20px] font-bold">{data?.name}</h3>
+                    <h4 className="tabletLg:text-[20px] text-[15px] opacity-50">{data?.original_name}</h4>
+                    <span className="text-decorTextColor tabletLg:text-[15px] text-[12px]">57 лет</span>
+                </div>
+            </div>
+
+            <div
+                className="absolute inset-0 bg-formElementColor bg-opacity-0 group-hover:bg-opacity-65
+                    flex items-center justify-center rounded-[10px] transition-all duration-300 ease-in-out">
+                <ButtonBase title={'People card'} onClick={() => alert('Переход на страницу...')}
+                            style="card"
+                            className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out"
+                />
             </div>
         </div>
+
+
     );
 };
