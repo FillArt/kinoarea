de07b35e90ec11e19233aaac924b9e84bb028185
Artiom Filipopovschii <artiom.filipopovschii@gmail.com>
2025-04-26 11:53:46 -0300

    feat(PopularPeople): added Empty element to handle the case when we do not receive a picture from the server

--- src/shared/ui/cards/CardMovie/Empty/Empty.tsx
+++ src/shared/ui/cards/CardMovie/Empty/Empty.tsx
@@ -1,13 +1,15 @@
 
 import EmptyIcon from '@/shared/ui/cards/CardMovie/Empty/assets/empty.svg'
 
-
-export const Empty = () => {
+type EmptyProps = {
+    noText?: boolean
+}
+export const Empty = ({noText = false}: EmptyProps) => {
     return (
         <div className="flex flex-col items-center text-center ">
             <img src={EmptyIcon} className="mb-6" alt="Empty poster"/>
-            <p className="text-descriptionFontSize text-emptyTextColor font-bold max-w-[235px]">К сожалению, Постер
-                отсутствует</p>
+            {!noText && <p className="text-descriptionFontSize text-emptyTextColor font-bold max-w-[235px]">К сожалению, Постер
+                отсутствует</p>}
         </div>
     );
 };
