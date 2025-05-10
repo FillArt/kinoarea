02ac51069e5082f696639bc91e549304bd87f460
Artiom Filipopovschii <artiom.filipopovschii@gmail.com>
2025-05-10 16:02:43 -0300

    feat(404): added mobile vesrion

--- src/pages/404/ui/NotFound.tsx
+++ src/pages/404/ui/NotFound.tsx
@@ -10,12 +10,12 @@ export const NotFound = () => {
     return (
         <WithoutSubscribeLayout>
             <div className="container text-center text-white ">
-                <div className="text-white font-black leading-tight mb-[26px]">
-                    <h1 className="text-[150px]">404</h1>
-                    <p className="text-[50px]">{t('title')}</p>
+                <div className="text-white font-black leading-tight phone:mb-[26px] mb-[15px]">
+                    <h1 className="phone:text-[150px] text-[100px]">404</h1>
+                    <p className="phone:text-[50px] text-[40px]">{t('title')}</p>
                 </div>
-                <p className="text-[18px] mb-[32px]">{t('subtitle')}</p>
-                <div className="flex gap-[15px] justify-center mb-[32px]">
+                <p className="phone:text-[18px] text-[15px] phone:mb-[32px] mb-[28px]">{t('subtitle')}</p>
+                <div className="flex phone:flex-row flex-col gap-[15px] max-w-auto justify-center mb-[32px]">
                     <ButtonBase
                         title={t('come_back')}
                         onClick={() => navigate("/kinoarea")}
