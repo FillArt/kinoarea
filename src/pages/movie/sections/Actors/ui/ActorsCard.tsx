91a2bffb1bcd287f7f649356a504574f3390f22f
Artiom Filipopovschii <artiom.filipopovschii@gmail.com>
2025-06-28 14:43:43 -0300

    feat(MoviePage): added a placeholder if there is no image

--- src/pages/movie/sections/Actors/ui/ActorsCard.tsx
+++ src/pages/movie/sections/Actors/ui/ActorsCard.tsx
@@ -1,4 +1,5 @@
 import {ActorType} from "@/shared/api/people/peopleType.ts";
+import EmptyIcon from '@/shared/ui/cards/CardMovie/Empty/assets/empty.svg'
 import {Link} from "react-router-dom";
 
 type IProps = {
@@ -8,9 +9,18 @@ type IProps = {
 export const ActorsCard = ({actor}: IProps) => {
     return (
         <div>
-            <div className=" w-full max-h-[368px] mb-[18px] rounded-[5px]">
-                <img className="max-h-[368px] w-auto object-contain rounded-[5px]" src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`} alt=""/>
-            </div>
+
+            {actor.profile_path ? (
+                <div className=" w-full max-h-[368px] mb-[18px] rounded-[5px]">
+                    <img className="max-h-[368px] w-auto object-contain rounded-[5px]"
+                         src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`} alt=""/>
+                </div>
+
+            ) : (
+                <div className="w-full flex justify-center items-center h-[368px] mb-[18px] rounded-[5px] bg-[#191E2E]">
+                    <img src={EmptyIcon} alt=""/>
+                </div>
+            )}
 
             <div>
                 <Link to="/">
